57b3d2ecdaf196b35d496f2ea932cb90f651c7a0
Sydney Erickson <sydneyerickson@icloud.com>
2017-04-21 22:30:09 -0700

    print core count for log

--- test.sh
+++ test.sh
@@ -7,21 +7,21 @@ do
         tar -xf busybox-1.26.2.tar.bz2
         cd busybox-1.26.2
         make defconfig
-        (time make -j1) &> ../busybox-$(date +%s).log
+        (time make -j1) &> ../busybox-$c-$(date +%s).log
         cd ..
         rm -rf busybox-1.26.2
 
         tar -xf linux-4.10.12.tar.xz
         cd linux-4.10.12
         make defconfig
-        (time make -j1) &> ../linux-$(date +%s).log
+        (time make -j1) &> ../linux-$c-$(date +%s).log
         cd ..
         rm -rf linux-4.10.12
 
         tar -xf coreutils-8.27.tar.xz
         cd coreutils-8.27
         ./configure
-        (time make -j1) &> ../coreutils-$(date +%s).log
+        (time make -j1) &> ../coreutils-$c-$(date +%s).log
         cd ..
         rm -rf coreutils-8.27
     done
