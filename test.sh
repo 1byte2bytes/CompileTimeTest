e97a09c2f82c533038ada7cbb4a5c890e20b2659
Sydney Erickson <sydneyerickson@icloud.com>
2017-04-21 22:29:24 -0700

    k

--- test.sh
+++ test.sh
@@ -1,30 +1,27 @@
 (getconf _NPROCESSORS_ONLN) &> cores.log
 
-for i in {1..8}
+for c in {1..8}
 do
     for i in {1..10}
     do
         tar -xf busybox-1.26.2.tar.bz2
         cd busybox-1.26.2
         make defconfig
-        (time make -j1) &> busybox-$(date +%s).log
-        mv *.log ..
+        (time make -j1) &> ../busybox-$(date +%s).log
         cd ..
         rm -rf busybox-1.26.2
 
         tar -xf linux-4.10.12.tar.xz
         cd linux-4.10.12
         make defconfig
-        (time make -j1) &> linux-$(date +%s).log
-        mv *.log ..
+        (time make -j1) &> ../linux-$(date +%s).log
         cd ..
         rm -rf linux-4.10.12
 
         tar -xf coreutils-8.27.tar.xz
         cd coreutils-8.27
         ./configure
-        (time make -j1) &> coreutils-$(date +%s).log
-        mv *.log ..
+        (time make -j1) &> ../coreutils-$(date +%s).log
         cd ..
         rm -rf coreutils-8.27
     done
