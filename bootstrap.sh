b57fdf0b9b80d1e2c540571833f32f7b6e0374d4
Sydney Erickson <sydneyerickson@icloud.com>
2017-07-07 01:15:47 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 sudo apt-get build-dep linux-image-`uname -r` busybox coreutils
 sudo apt-get install curl build-essential
-curl https://www.busybox.net/downloads/busybox-1.26.2.tar.bz2 > busybox-1.26.2.tar.bz2
-curl https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.10.12.tar.xz > linux-4.10.12.tar.xz
+curl https://www.busybox.net/downloads/busybox-1.27.0.tar.bz2 > busybox-1.26.2.tar.bz2
+curl https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.12.tar.xz > linux-4.10.12.tar.xz
 curl https://ftp.gnu.org/gnu/coreutils/coreutils-8.27.tar.xz > coreutils-8.27.tar.xz
