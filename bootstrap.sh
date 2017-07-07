6df1087b16d8f036aee815a49c3c920f20e727ba
Sydney Erickson <sydneyerickson@icloud.com>
2017-07-07 10:06:10 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 sudo apt-get build-dep linux-image-`uname -r` busybox coreutils cmake
 sudo apt-get install curl build-essential
-curl https://www.busybox.net/downloads/busybox-1.27.0.tar.bz2 > busybox-1.26.2.tar.bz2
-curl https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.12.tar.xz > linux-4.10.12.tar.xz
+curl https://www.busybox.net/downloads/busybox-1.27.0.tar.bz2 > busybox-1.27.0.tar.bz2
+curl https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.12.tar.xz > linux-4.12.tar.xz
 curl https://ftp.gnu.org/gnu/coreutils/coreutils-8.27.tar.xz > coreutils-8.27.tar.xz
 curl http://releases.llvm.org/4.0.1/llvm-4.0.1.src.tar.xz > llvm-4.0.1.src.tar.xz
 curl http://releases.llvm.org/4.0.1/cfe-4.0.1.src.tar.xz > cfe-4.0.1.src.tar.xz
