d7b6a5b6dd3121e4a96c4de1251b591feb9ddae3
Sydney Erickson <sydneyerickson@icloud.com>
2017-07-07 09:45:39 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -27,5 +27,24 @@ do
         (time make -j$c) &> ../coreutils-$c-$(date +%s).log
         cd ..
         rm -rf coreutils-8.27
+        
+        echo CLANG CORE COUNT $c - TRY COUNT $i
+        tar -xf llvm-4.0.1.src.tar.xz
+        tar -xf cfe-4.0.1.src.tar.xz
+        tar -xf clang-tools-extra-4.0.1.src.tar.xz
+        tar -xf compiler-rt-4.0.1.src.tar.xz
+        
+        mv llvm-4.0.1.src llvm
+        mv cfe-4.0.1.src llvm/tools/clang
+        mv clang-tools-extra-4.0.1.src llvm/tools/clang/tools/extra
+        mv compiler-rt-4.0.1.src llvm/projects/compiler-rt
+        
+        mkdir build-clang
+        cd build-clang
+        (cmake -G "Unix Makefiles" ../llvm) &> /dev/null
+        (time make -j$c) &> ../clang-$c-$(date +%s).log
+        cd ..
+        rm -rf llvm
+        rm -rf build-clang
     done
 done
