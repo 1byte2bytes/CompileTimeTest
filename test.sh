472f10f0de46ad44d7f91b4d9c046568184761eb
Sydney Erickson <sydneyerickson@icloud.com>
2017-07-07 10:03:13 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,24 +1,24 @@
 (nproc) &> cores.log
 
-for c in {1..$(nproc)}
+for c in {1..8}
 do
     for i in {1..10}
     do
         echo BUSYBOX CORE COUNT $c - TRY COUNT $i
-        tar -xf busybox-1.26.2.tar.bz2
-        cd busybox-1.26.2
+        tar -xf busybox-1.27.0.tar.bz2
+        cd busybox-1.27.0
         (make defconfig) &> /dev/null
         (time make -j$c) &> ../busybox-$c-$(date +%s).log
         cd ..
-        rm -rf busybox-1.26.2
+        rm -rf busybox-1.27.0
 
         echo LINUX CORE COUNT $c - TRY COUNT $i
-        tar -xf linux-4.10.12.tar.xz
-        cd linux-4.10.12
+        tar -xf linux-4.12.tar.xz
+        cd linux-4.12
         (make defconfig) &> /dev/null
         (time make -j$c) &> ../linux-$c-$(date +%s).log
         cd ..
-        rm -rf linux-4.10.12
+        rm -rf linux-4.12
 
         echo COREUTILS CORE COUNT $c - TRY COUNT $i
         tar -xf coreutils-8.27.tar.xz
