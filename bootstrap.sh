0f473b19042516c1035ab14cd19ca1401d867bff
Sydney Erickson <sydneyerickson@icloud.com>
2017-07-27 14:45:37 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-sudo apt-get build-dep linux-image-`uname -r` busybox coreutils cmake python2.7 python3
+sudo apt-get build-dep linux-image-`uname -r` busybox coreutils cmake python2.7 python3 bc
 sudo apt-get install curl build-essential
 curl https://www.busybox.net/downloads/busybox-1.27.0.tar.bz2 > busybox-1.27.0.tar.bz2
 curl https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.12.tar.xz > linux-4.12.tar.xz
