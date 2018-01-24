bb3f9c7418705900e00e2e9c6e2f2dc4b5061f5e
Sydney Erickson <sydneyerickson@icloud.com>
2018-01-23 21:19:58 -0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,10 +5,10 @@ for c in {1..8}
 do
     for i in {1..10}
     do
-        echo BUSYBOX CORE COUNT $c - TRY COUNT $i
         tar -xf busybox-1.27.0.tar.bz2
         cd busybox-1.27.0
-        (make defconfig) &> /dev/null
+        make defconfig
+        echo BUSYBOX CORE COUNT $c - TRY COUNT $i
         (/usr/bin/time -f "%E %P %M" make -j$c) &> ../busybox-$c-$(date +%s).log
         cd ..
         rm -rf busybox-1.27.0
@@ -21,26 +21,26 @@ do
         #cd ..
         #rm -rf linux-4.12
 
-        echo COREUTILS CORE COUNT $c - TRY COUNT $i
         tar -xf coreutils-8.27.tar.xz
         cd coreutils-8.27
-        (./configure) &> /dev/null
+        ./configure
+        echo COREUTILS CORE COUNT $c - TRY COUNT $i
         (/usr/bin/time -f "%E %P %M" make -j$c) &> ../coreutils-$c-$(date +%s).log
         cd ..
         rm -rf coreutils-8.27
         
-        echo PYTHON 3 CORE COUNT $c - TRY COUNT $i
         tar -xf Python-3.6.1.tgz
         cd Python-3.6.1
-        (./configure) &> /dev/null
+        ./configure
+        echo PYTHON 3 CORE COUNT $c - TRY COUNT $i
         (/usr/bin/time -f "%E %P %M" make -j$c) &> ../python3-$c-$(date +%s).log
         cd ..
         rm -rf Python-3.6.1
         
-        echo PYTHON 2 CORE COUNT $c - TRY COUNT $i
         tar -xf Python-2.7.13.tgz
         cd Python-2.7.13
-        (./configure) &> /dev/null
+        ./configure
+        echo PYTHON 2 CORE COUNT $c - TRY COUNT $i
         (/usr/bin/time -f "%E %P %M" make -j$c) &> ../python2-$c-$(date +%s).log
         cd ..
         rm -rf Python-2.7.13
